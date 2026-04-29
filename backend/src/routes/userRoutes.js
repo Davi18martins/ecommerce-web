f6febd1a71432bd2b898432ac844f47db7cd8f24
Davi Martins <davi.jesus@alunos.ifsuldeminas.edu.br>
2026-04-29 12:14:21 -0300

    atualização

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 const controller = require("../controllers/userController")
-const auth = require("../middleware/authMiddleware")
+const auth = require("../../middleware/authMiddleware")
 
 router.post("/register", controller.register)
 router.get("/profile", auth, controller.getProfile)
